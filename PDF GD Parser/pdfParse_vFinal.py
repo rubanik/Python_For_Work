d1254ed31c3f14ca508fba077f1e28d1a3c44a14
rubanik <anton.rubanik@gmail.com>
2021-08-22 19:39:49 +0300

    Tested from home

--- PDF GD Parser/pdfParse_vFinal.py
+++ PDF GD Parser/pdfParse_vFinal.py
@@ -7,17 +7,16 @@ from openpyxl import Workbook
 def get_names_list():
     #Смотрим какие файлы есть в текущем каталоге
     name_list = []
-    for name in os.listdir(): 
+    for name in os.listdir():
         if 'Liste' in name: # Если в имени есть Liste - значит это наш клиент
             name_list.append(name)
     print('Обнаружены файлы:', name_list,'\n')
 
     return name_list
-    
+
 
 def get_lines_from_pdf(path):
     # Парсим построчно пдф файл и отсееваем мусор
-    
     pdfFile = open(path,'rb')
     prdReader = PyPDF2.PdfFileReader(pdfFile)
     num = prdReader.getNumPages()
@@ -43,9 +42,6 @@ def get_lines_from_pdf(path):
     pdfFile.close()
     return result
 
-    pdfFile.close()
-    return result
-
 def get_lines_from_pdf_v2(path):
 
     # Парсим построчно пдф файл и отсееваем мусор
@@ -62,7 +58,6 @@ def get_lines_from_pdf_v2(path):
     for page in range(num):
         page_act = prdReader.getPage(page)
         text_act = page_act.extractText()
-        
         for line in io.StringIO(text_act):
             linez = line.split('\n')
             if re.fullmatch(r'\d{1} ',linez[0][-2:]):
@@ -77,10 +72,9 @@ def get_lines_from_pdf_v2(path):
 
 
 def change_empty_name(last,current):
-
     new_line_with_name = ' ' + last.split()[0] + current[len(last.split()[0])+1:]
     return new_line_with_name
-   
+
 #get_lines_from_pdf_v2('FR1618503_002_78_Liste_Catalogo.pdf')
 
 
